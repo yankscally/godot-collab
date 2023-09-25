7af2cb2411c181edb04c2a48d26d2881c97b9ee9
yankscally <yankscally@gmail.com>
2023-09-25 18:37:27 +0100

    split projects into client server

---
+++ server project/addons/godot-collab/rpc.gd
@@ -0,0 +1,22 @@
+@tool
+extends Node
+var script_editor_init = EditorPlugin.new()
+
+var script_source 
+
+
+@rpc("any_peer", "unreliable")
+func get_scriptlist(scriptlist, main):
+	var script_editor = script_editor_init.get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
+	main = get_node_or_null(main)
+	if main != null:
+		for each in scriptlist:
+			if not "addons" in each:
+
+				if not each in main.editor.script_list:
+					main.editor.script_list[each] = load(each)
+				
+				main.editor.script_list[each].source_code = scriptlist[each]
+				
+				print(scriptlist[each])
+				
