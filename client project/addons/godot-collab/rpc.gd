9f75f96d895ccf1514e70136f8fd8b83f45a9c11
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2023-09-26 03:15:47 +0100

    Update rpc.gd

--- client project/addons/godot-collab/rpc.gd
+++ client project/addons/godot-collab/rpc.gd
@@ -1,17 +1,18 @@
 @tool
 extends Node
+var script_editor
 
 var script_source 
-var script_editor_connected = false
 
-var updating = false
+
+func _ready():
+	var editor = EditorPlugin.new()
+	script_editor = editor.get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
+	script_editor.connect("text_changed", update_script)
 
 @rpc("any_peer", "unreliable")
 func get_scriptlist(scriptlist):
-	var script_editor_init = EditorPlugin.new()
-	var script_editor = script_editor_init.get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
-	make_connections()
-	
+	## tool is meta on this node, it shouold ref collab_tool
 	var main = get_meta("tool")
 	for each in scriptlist:
 		if not "addons" in each:
@@ -20,25 +21,7 @@ func get_scriptlist(scriptlist):
 			main.editor.script_list[each].source_code = scriptlist[each]
 			
 			script_source = scriptlist[each]
-
-
-
 			update_script(script_source)
 
-
-
-func make_connections():
-	var script_editor_init = EditorPlugin.new()
-	var script_editor = script_editor_init.get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
-	
-	if script_editor_connected == false:
-		script_editor.connect("text_entered", update_script)
-		script_editor_connected = true
-		
 func update_script(input):
-	updating = true
-	if updating == true:
-		var script_editor_init = EditorPlugin.new()
-		
-		script_editor_init.get_editor_interface().get_script_editor().get_current_editor().get_base_editor().set("text", input)
-		updating = false
+		script_editor.set("text", input)
